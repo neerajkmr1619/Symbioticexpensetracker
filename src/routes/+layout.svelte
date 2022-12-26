017f8cfc5c570d349de2d31f0f93941eba490bcc
Sandy Singh <sandy.singh8080@outlook.com>
2022-12-26 13:57:22 -0500

    skeleton of site added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
 	import '../app.postcss';
+	import Header from './Components/header.svelte';
 </script>
 
-<slot />
+<Header/>
+
+<div class="drawer drawer-mobile">
+	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col items-center justify-center">
+	  <!-- Page content here -->
+	  <slot />
+	  <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+	
+	</div> 
+	<div class="drawer-side">
+	  <label for="my-drawer-2" class="drawer-overlay"></label> 
+	  <ul class="menu p-4 w-80 bg-base-300 text-base-content">
+		<!-- Sidebar content here -->
+		<li><a href = "/">Dashboard</a></li>
+		<li><a href = "/Submit">Submit</a></li>
+		<li><a href = "/View">View</a></li>
+	  </ul>
+	
+	</div>
+  </div>
+
+
