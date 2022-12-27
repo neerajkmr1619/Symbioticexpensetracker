38efa944334305c4edaf59c090e637694c70168c
neeraj kumar <neeraj.kumar@vitas.com>
2022-12-27 15:53:29 -0500

    Refactored table data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,20 +4,30 @@
 	import { goto } from '$app/navigation';
 
     export let data;
-    //const {products} = data;
-    //console.log(products);
-    //setting store
-    //productsData.set(products);
+    
+    let dashboadItems;
+    let name = '';
 
-    const viewItem = (rowId) => {
-       console.log("/View/"+rowId.id);
-       goto("/View/"+rowId.id);
+    const unsubscribe = productsData.subscribe(value => {
+		  dashboadItems = value;
+	});  
+
+    const  viewItem   = (rowId) => {
+          console.log("/View/"+rowId.id);
+          goto("/View/"+rowId.id);
     }
+
+    const filterDashboardItems = () => {
+          dashboadItems = dashboadItems.filter( x => x.title.startsWith(name))
+    }
+
 </script>
 
 <!-- <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-<h1>All your submitted expenses</h1>
+<h1>All your submitted expenses</h1> <p>Hello {name || 'stranger'}!</p>
+<input type="text" bind:value={name} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+<button class="btn btn-outline btn-primary" on:click={filterDashboardItems}>Button</button>
     <br/>
 <div class="overflow-x-auto w-full">
     
@@ -32,7 +42,7 @@
         </tr>
       </thead>
       <tbody>
-        {#each $productsData as {id,title,description,price}}        
+        {#each dashboadItems as {id,title,description,price}}        
         <!-- rows -->
         <tr class="hover" on:click|preventDefault={()=>viewItem({id})}>
           <th>{id}</th>
